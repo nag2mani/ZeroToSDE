cf0f482cc77983d3b7aebbda7568b8b177280179
Nagmani kumar <115435469+nag2mani@users.noreply.github.com>
2025-08-29 08:43:23 +0530

    Update BubbleSort.java

--- StriverAZ/2.sorting/BubbleSort.java
+++ StriverAZ/2.sorting/BubbleSort.java
@@ -8,7 +8,6 @@ public class BubbleSort {
     }
 
     public static void bubble_sort(int arr[]) {
-        System.out.println("Original Array: " + Arrays.toString(arr));
         for (int k = 0; k < arr.length; k++) {
             int i = 0;
             int j = 1;
@@ -20,7 +19,6 @@ public class BubbleSort {
                 j++;
             }
         }
-        System.out.println("Sorted Array: " + Arrays.toString(arr));
     }
 
     public static void optimised_bubble_sort(int arr[]) {
@@ -37,7 +35,7 @@ public class BubbleSort {
                 i++;
                 j++;
             }
-            System.out.println(noOfSwaps);
+            // System.out.println(noOfSwaps);
             if (noOfSwaps == 0) {
                 break;
             }
@@ -45,10 +43,30 @@ public class BubbleSort {
         System.out.println("Sorted Array: " + Arrays.toString(arr));
     }
 
+    public static void recursive_bubble_sort(int[] arr, int n){
+        if (n == 1) {
+            return;
+        }
+        int i = 0;
+        int j = 1;
+        while (i < j && j < arr.length) {
+            if (arr[i] > arr[j]) {
+                swap(arr, i, j);
+            }
+            i++;
+            j++;
+        }
+        recursive_bubble_sort(arr, n - 1);
+}
+
     public static void main(String[] args) {
-        // int[] arr = { 13, 46, 11, -1, 24, -52, 20, 9, 0, 2, 2 };
-        int[] arr = { 9, 46, 24, 52, 20, 13 };
+        int[] arr = { 13, 46, 11, -1, 24, -52, 20, 9, 0, 2, 2 };
+        // int[] arr = { 9, 46, 24, 52, 20, 13 };
+        int n = arr.length;
+        System.out.println("Original Array: " + Arrays.toString(arr));
         // bubble_sort(arr);
-        optimised_bubble_sort(arr);
+        // optimised_bubble_sort(arr);
+        recursive_bubble_sort(arr, n);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
     }
 }
