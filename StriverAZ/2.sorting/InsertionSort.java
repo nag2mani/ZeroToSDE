8b96d342bcb0dfe445c777257eb1d190215851df
Nagmani kumar <115435469+nag2mani@users.noreply.github.com>
2025-08-29 08:43:53 +0530

    Update InsertionSort.java

--- StriverAZ/2.sorting/InsertionSort.java
+++ StriverAZ/2.sorting/InsertionSort.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 
 public class InsertionSort {
-
     public static void swap(int arr[], int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
@@ -21,7 +20,6 @@ public class InsertionSort {
     }
 
     public static void insertion_sort(int[] arr){
-        System.out.println("Original Array: " + Arrays.toString(arr));
         for (int i=1; i<arr.length;i++){
             int pos = insertion_pos(arr, i);
             int j = i;
@@ -30,12 +28,29 @@ public class InsertionSort {
                 j--;
             }
         }
-        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+
+    public static void recursive_insertion_sort(int[] arr, int k, int n){
+        if (k==n){
+            return;
+        }
+        int i = k;
+            int pos = insertion_pos(arr, i);
+            int j = i;
+            while (j>pos) {
+                swap(arr, j, j-1);
+                j--;
+            }
+        recursive_insertion_sort(arr,k+1, n);
     }
 
     public static void main(String[] args) {
-        // int[] arr = { 13, 46, 11, -1, 24, -52, 20, 9, 0, 2, 2 };
-        int[] arr = {29,10,14,37,13};
-        insertion_sort(arr);
+        int[] arr = { 13, 46, 11, -1, 24, -52, 20, 9, 0, 2, 2 };
+        // int[] arr = { 9, 46, 24, 52, 20, 13 };
+        int n = arr.length;
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        // insertion_sort(arr);
+        recursive_insertion_sort(arr, 1, n);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
     }
 }
