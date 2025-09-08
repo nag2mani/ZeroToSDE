f7dbd37b530550b45e712370cae259c1b2ef3df9
Nagmani Kumar <nag2mani@gmail.com>
2025-09-08 20:21:47 +0530

    added xor wala

--- StriverAZ/3.array/easy/findMissingNumber.java
+++ StriverAZ/3.array/easy/findMissingNumber.java
@@ -9,4 +9,18 @@ public class findMissingNumber {
         }
         System.err.println(actualSum-sum);
     }
+    public static int findMissingNumberXOR(int[] arr, int n) {
+        int xor1 = 0;
+        int xor2 = 0;
+        // XOR all array elements
+        for (int i = 0; i < arr.length; i++) {
+            xor1 ^= arr[i];
+        }
+        // XOR all numbers from 1 to n
+        for (int i = 1; i <= n; i++) {
+            xor2 ^= i;
+        }
+        // Missing number is xor1 ^ xor2
+        return xor1 ^ xor2;
+    }
 }
