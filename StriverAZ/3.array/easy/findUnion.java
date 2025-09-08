94c42daabe37e1651c3ee3e6e383d9138ebb569f
Nagmani Kumar <nag2mani@gmail.com>
2025-09-08 19:23:03 +0530

    done union

---
+++ StriverAZ/3.array/easy/findUnion.java
@@ -0,0 +1,28 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class findUnion {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 4, 5, 6};
+        int[] arr2 = {2, 3, 5, 7};
+        findUnions(arr1, arr2);
+    }
+
+    public static void findUnions(int[] arr1, int[] arr2){
+        Set<Integer> set = new LinkedHashSet<>();
+        int i =0;
+        int j=0;
+        int m = arr1.length;
+        int n = arr2.length;
+        while (i < m || j < n) {
+            if (i < m && arr1[i] < arr2[j]) {
+                set.add(arr1[i]);
+                i++;
+            }else{
+                set.add(arr2[j]);
+                j++;
+            }
+        }
+        System.out.println(set);
+    }
+}
---
+++ StriverAZ/3.array/easy/linearSearch.java
@@ -0,0 +1,12 @@
+public class linearSearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,0,1,0,4,0};
+        int target = 0;
+        // System.out.println(linearSearchs(arr, 4));
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==target) {
+                System.err.println(i);
+            }
+        }
+    }
+}
