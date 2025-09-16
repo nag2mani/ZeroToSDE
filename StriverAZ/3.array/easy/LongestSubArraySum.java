e1bbfff2dbb9e4525079df3f998d2815da78be70
Nagmani Kumar <nag2mani@gmail.com>
2025-09-16 16:27:14 +0530

    updated

--- StriverAZ/3.array/easy/LongestSubArraySum.java
+++ StriverAZ/3.array/easy/LongestSubArraySum.java
@@ -1,5 +1,15 @@
 public class LongestSubArraySum {
     public static void main(String[] args) {
-        
+        int arr[] = {5,5,2,3,5,1,9};
+        int target = 10;
+        System.out.println("Length of longest subarray with sum 0 is: " + findLongestSubarray(arr, target));
+    }
+
+    public static int findLongestSubarraySum(int[] arr, int target){
+        int i = 0;
+        int j = 0;
+        while (condition) {
+            
+        }
     }
 }
