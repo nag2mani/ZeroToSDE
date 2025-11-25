08d3c8488346343bc844617660222faf291984ab
Nagmani Kumar <nag2mani@gmail.com>
2025-11-25 20:05:38 +0530

    done easy and medium

--- striver/striver_az/array/medium/PrintMatrix.java
+++ striver/striver_az/array/medium/PrintMatrix.java
@@ -1,5 +1,51 @@
 package striver.striver_az.array.medium;
+
 // Given an m x n matrix, return all elements of the matrix in spiral order.
 public class PrintMatrix {
-    
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 }
+        };
+        printSprialMatrix(matrix);
+    }
+
+    public static void printSprialMatrix(int[][] matrix) {
+        int left = 0;
+        int right = matrix[0].length - 1;
+        int top = 0;
+        int bottom = matrix.length - 1;
+
+        while (left <= right && top <= bottom) {
+
+            // Print left to right.(cols change)
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[top][i]);
+            }
+            top++;
+
+            // print top to bottom (row change)
+            for (int j = top; j <= bottom; j++) {
+                System.out.print(matrix[j][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                // print right to left
+                for (int k = right; k >= left; k--) {
+                    System.out.print(matrix[bottom][k]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                // print bottom to top
+                for (int l = bottom; l >= top; l--) {
+                    System.out.print(matrix[l][left]);
+                }
+                left++;
+            }
+        }
+    }
 }
--- striver/striver_az/array/medium/RotateMatrixBy.java
+++ striver/striver_az/array/medium/RotateMatrixBy.java
@@ -1,5 +1,49 @@
 package striver.striver_az.array.medium;
-
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise). IN-Place
+import java.util.Arrays;
 public class RotateMatrixBy {
-    
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 2, 3},
+                { 4, 5, 6},
+                { 7, 8, 9}
+        };
+        RotateMatrixByX(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+
+    // public static int[][] RotateMatrixByX(int[][] matrix){
+    //     int[][] matrix1={{0,0,0}, {0,0,0}, {0,0,0}};
+    //     for (int i = 0; i < matrix.length; i++) {
+    //         for (int j = 0; j < matrix[0].length; j++) {
+    //             int temp = matrix[i][j];
+    //             matrix1[j][matrix.length - 1 - i]= temp;
+    //         }
+    //     }
+    //     return matrix1;
+    // }
+
+    public static void RotateMatrixByX(int[][] matrix){
+        // Solving Inplace (Transpose the matrix and reverse)
+
+        // Transpose
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[0].length; j++) { // this will reverse trangle (not whole)
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // Reverse
+        for (int i = 0; i < matrix.length; i++) {
+            int left = 0, right = matrix.length - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
 }
